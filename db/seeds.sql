cdde95bd314d921832dc1acada5baee00dd20448
Karen Acosta <kar3n.acosta@gmail.com>
2021-01-16 22:30:49 -0600

    adding seeds.sql file

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoked Crispy Skin Salmon Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Delight Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Lovers Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Lettuce Bun Burger of the day!', true);
+
